a8044c079d403d9f175a85393c86ffa4dba105a8
Kridsada-Wannasing <kridsada_aex002@hotmail.com>
2021-10-27 23:07:45 +0700

    edit file path

--- src/users/users.service.ts
+++ src/users/users.service.ts
@@ -2,7 +2,7 @@ import { HttpException, HttpStatus, Injectable } from '@nestjs/common';
 import { InjectRepository } from '@nestjs/typeorm';
 import { Repository } from 'typeorm';
 import { CreateUserDto } from './dto/create-user.dto';
-import User from './entities/user.entity';
+import User from './user.entity';
 
 @Injectable()
 export class UsersService {
