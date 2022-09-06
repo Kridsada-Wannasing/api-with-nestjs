f65e97aacb7cf263db8b76729ecf1b5c64f2253a
Kridsada-Wannasing <kridsada_aex002@hotmail.com>
2022-09-06 12:26:03 +0700

    feat: set redis cache and multiple nestjs instance

--- src/posts/posts.module.ts
+++ src/posts/posts.module.ts
@@ -5,12 +5,23 @@ import { TypeOrmModule } from '@nestjs/typeorm';
 import { PostsSearchService } from './posts-search.service';
 import Post from './post.entity';
 import { SearchModule } from '../search/search.module';
+import { ConfigModule, ConfigService } from '@nestjs/config';
+import * as redisStore from 'cache-manager-redis-store';
 
 @Module({
   imports: [
     TypeOrmModule.forFeature([Post]),
     SearchModule,
-    CacheModule.register(),
+    CacheModule.registerAsync({
+      imports: [ConfigModule],
+      inject: [ConfigService],
+      useFactory: (configService: ConfigService) => ({
+        store: redisStore,
+        host: configService.get('REDIS_HOST'),
+        port: configService.get('REDIS_PORT'),
+        ttl: 120,
+      }),
+    }),
   ],
   controllers: [PostsController],
   providers: [PostsService, PostsSearchService],
