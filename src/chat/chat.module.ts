046c4a563f34016b07086af7db4c7c84cdb39fd3
Kridsada-Wannasing <kridsada_aex002@hotmail.com>
2022-10-03 13:33:22 +0700

    fix: setting message entity

--- src/chat/chat.module.ts
+++ src/chat/chat.module.ts
@@ -2,9 +2,11 @@ import { Module } from '@nestjs/common';
 import { ChatService } from './chat.service';
 import { ChatGateway } from './chat.gateway';
 import { AuthModule } from '../auth/auth.module';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import Message from './message.entity';
 
 @Module({
-  imports: [AuthModule],
+  imports: [AuthModule, TypeOrmModule.forFeature([Message])],
   providers: [ChatService, ChatGateway],
 })
 export class ChatModule {}
